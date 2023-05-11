66e329a9fa93f794a9270923e490765fc8ba6cdc
riko <harukikawakita3@gmail.com>
2023-05-11 21:59:48 +0900

    Pandas

--- Pandas.py
+++ Pandas.py
@@ -74,3 +74,70 @@
 
 # # グラフを表示
 # plt.show()
+
+import pandas as pd
+
+# df = pd.read_csv('file.csv', names=['Name', 'Year', 'Team', 'Points'])
+# print(df)
+
+# df1 = pd.DataFrame({'A': ['A0', 'A1', 'A2', 'A3'],
+#                     'B': ['B0', 'B1', 'B2', 'B3'],
+#                     'C': ['C0', 'C1', 'C2', 'C3'],
+#                     'D': ['D0', 'D1', 'D2', 'D3']})
+
+# df2 = pd.DataFrame({'A': ['A4', 'A5', 'A6', 'A7'],
+#                     'B': ['B4', 'B5', 'B6', 'B7'],
+#                     'C': ['C4', 'C5', 'C6', 'C7'],
+#                     'D': ['D4', 'D5', 'D6', 'D7']})
+
+# ddf = pd.concat([df1, df2])
+# print(ddf)
+
+# data = {'Name': ['Michael Jordan', 'Kobe Bryant', 'Kevin Durant', 'LeBron James', 'Stephen Curry', 'James Harden'],
+#         'Year': [1996, 2006, 2014, 2020, 2010, 2009],
+#         'Team': ['Chicago Bulls', 'Los Angeles Lakers', 'Oklahoma City Thunder', 'Los Angeles Lakers', 'Golden State Warriors', 'Houston Rockets'],
+#         'Points': [2491, 2832, 2593, 1698, 2626, 2191]}
+
+# df = pd.DataFrame(data)
+
+# meanPoints = df.groupby(['Team'])['Points'].mean()
+# meanDf = pd.DataFrame(meanPoints).reset_index()
+# print(meanDf)
+
+# import pandas as pd
+
+# data = {'Name': ['Michael Jordan', 'Kobe Bryant', 'Kevin Durant', 'LeBron James', 'Stephen Curry', 'James Harden'],
+#         'Year': [1996, 2006, 2014, 2020, 2010, 2009],
+#         'Team': ['Chicago Bulls', 'Los Angeles Lakers', 'Oklahoma City Thunder', 'Los Angeles Lakers', 'Golden State Warriors', 'Houston Rockets'],
+#         'Points': [2491, 2832, 2593, 1698, 2626, 2191]}
+
+# df = pd.DataFrame(data)
+
+# df['Name'] = 'Sr.' + df['Name']
+# print(df)
+
+# import pandas as pd
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+
+# # データをDataFrameに格納する
+# data = {'Name': ['Michael Jordan', 'Kobe Bryant', 'Kevin Durant', 'LeBron James', 'Stephen Curry', 'James Harden'],
+#         'Year': [1996, 2006, 2014, 2020, 2010, 2009],
+#         'Team': ['Chicago Bulls', 'Los Angeles Lakers', 'Oklahoma City Thunder', 'Los Angeles Lakers', 'Golden State Warriors', 'Houston Rockets'],
+#         'Points': [2491, 2832, 2593, 1698, 2626, 2191]}
+# df = pd.DataFrame(data)
+
+# # Teamごとの年間得点数を計算する
+# # team_points = df.groupby('Team')['Points'].sum()
+
+# # ヒストグラムを作成して表示する
+
+# plt.figure()
+# plt.rcParams['axes.prop_cycle'] = plt.cycler(color=plt.cm.Set3.colors)
+
+# sns.histplot(data=df, x="Points", hue="Team", bins=30)
+# plt.title('Yearly Point Distribution by Teams')
+# plt.xlabel('Points')
+# plt.ylabel('Counts')
+# plt.legend()
+# plt.show()
